a0a29398d9bef4ab4316cdab94c9a1c0dd2051bc
Mattvers <mateusz.grzegorzewicz@gmail.com>
2017-06-26 15:37:46 +0200

    Empty project with updates

---
+++ ServiceClassFields/ServiceClassFields/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ServiceClassFields.Startup))]
+namespace ServiceClassFields
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
