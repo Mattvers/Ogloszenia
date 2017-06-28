62a22b93359ecca1fe310aa3a3f9859ac14d59c3
Mattvers <mateusz.grzegorzewicz@gmail.com>
2017-06-28 13:29:48 +0200

    Add User model and basic validation

--- ServiceClassFields/ServiceClassFields/Models/Announcement_Category.cs
+++ ServiceClassFields/ServiceClassFields/Models/Announcement_Category.cs
@@ -7,5 +7,11 @@ namespace ServiceClassFields.Models
 {
     public class Announcement_Category
     {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int AnnouncementId { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual Announcement Announcement { get; set; }
     }
 }
--- ServiceClassFields/ServiceClassFields/Models/User.cs
+++ ServiceClassFields/ServiceClassFields/Models/User.cs
@@ -2,10 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ServiceClassFields.Models
 {
-    public class User
+    public class User : IdentityUser
     {
+        public User()
+        {
+            this.Announcement = new HashSet<Announcement>();
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        #region notmappedField
+
+        [NotMapped]
+        [Display(Name = "Pan/Pani:")]
+        public string FullName
+        {
+            get
+            {
+                return Name + " " + Surname;
+            }
+        }
+
+        #endregion
+
+        public virtual ICollection<Announcement> Announcement { get; private set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+
     }
 }
